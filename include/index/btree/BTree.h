710c4c679db915dd9a5e3196093f4f337fbbcdd0
Anirudh Sowmyanarayanan <anikrish1998@gmail.com>
2022-04-10 23:17:04 -0400

    l commit

--- include/index/btree/BTree.h
+++ include/index/btree/BTree.h
@@ -9,6 +9,7 @@
 #include "index/IndexKey.h"
 #include "storage/Record.h"
 #include "storage/FileManager.h"
+#include "storage/VarlenDatapage.h"
 
 namespace taco {
 
@@ -226,6 +227,8 @@ private:
      * Note that it is undefined \p find_next_key is true when \p buf points to
      * an internal page.
      */
+
+
     SlotId BinarySearchOnPage(char *buf,
                               const IndexKey *key,
                               const RecordId &recid);
